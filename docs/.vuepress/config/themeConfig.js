9689dc40158d1b4616ff5b4fc84b58669a1036fc
王杰 <jwang89@leqee.com>
2023-06-09 21:51:47 +0800

    :tada:

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -5,7 +5,7 @@ module.exports = {
   nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: '/img/小天鹅.svg', // 导航栏logo
-  repo: 'felix-wj/MyBlog', // 导航栏右侧生成Github链接
+  repo: 'felix-wj/felix-wj.github.io.git', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
