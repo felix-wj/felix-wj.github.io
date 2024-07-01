34198f8b5e093a95fa42e4dc2d4faca564d91314
felix-wj <1358726405@qq.com>
2024-07-01 15:50:08 +0800

    Update GitHub Pages deployment workflow

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+pnpm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# echo 'www.isunbeam.cn' > CNAME
+echo 'blog.edwsmycs.cn' > CNAME
 
 git init
 git add -A
